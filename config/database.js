3d43179451c92f1686c73cbc91edf3fbc87969a9
Yondo202 <yondooo61@gmail.com>
2022-03-06 21:16:09 +0800

    dd

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '192.168.88.78'),
+      // host: env('DATABASE_HOST', '192.168.88.78'),
+      host: env('DATABASE_HOST', '124.158.107.34'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'edumedia'),
       user: env('DATABASE_USERNAME', 'root'),
