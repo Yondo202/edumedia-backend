17f427c00c49b2334e3c55fe19886f4f78ffdd61
Yondo202 <yondooo61@gmail.com>
2022-03-16 13:23:28 +0800

    dd

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,10 @@
 // path: ./config/plugins.js
 
-module.exports = {
-    // ...
-    "quill-editor": {
-      enabled: true,
-      resolve: "./src/plugins/quill-editor", // path to plugin folder
-    },
-    // ...
-};
+// module.exports = {
+//     // ...
+//     "quill-editor": {
+//       enabled: true,
+//       resolve: "./src/plugins/quill-editor", // path to plugin folder
+//     },
+//     // ...
+// };
