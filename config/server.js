60357cd9f7cfb7a92b8bf5112cb5150c36e5dacc
infosystems-mn <contact@infosystems.mn>
2022-03-18 11:03:45 +0800

    url add

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1380),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1380),
+  url: "https://edumedia.mn/api",
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
